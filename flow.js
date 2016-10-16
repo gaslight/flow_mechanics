fbea2988948317f527af8d5b69b29bbd91a3c311
Peter Kananen <peter@teamgaslight.com>
2016-10-16 16:43:01 -0400

    show queue sizes

--- flow.js
+++ flow.js
@@ -4,50 +4,62 @@ angular.module('flowApp', [])
 
     flow.columns = [
         {name: 'Backlog', id: 1, start: true, end: false, idle: true, wipLimit: 99},
-        {name: 'Up Next', id: 2, start: false, end: false, idle: false, wipLimit: 4},
+        {name: 'Rqmts', id: 2, start: false, end: false, idle: false, wipLimit: 1},
         {name: 'RF Desgn', id: 3, start: false, end: false, idle: true, wipLimit: 4},
         {name: 'Design', id: 4, start: false, end: false, idle: false, wipLimit: 1},
         {name: 'RF Dev', id: 5, start: false, end: false, idle: true, wipLimit: 4},
         {name: 'Dev', id: 6, start: false, end: false, idle: false, wipLimit: 1},
-        {name: 'RF UI', id: 7, start: false, end: false, idle: true, wipLimit: 4},
-        {name: 'UI', id: 8, start: false, end: false, idle: false, wipLimit: 1},
-        {name: 'RF QA', id: 9, start: false, end: false, idle: true, wipLimit: 4},
-        {name: 'QA', id: 10, start: false, end: false, idle: false, wipLimit: 1},
+        {name: 'RF QA', id: 7, start: false, end: false, idle: true, wipLimit: 4},
+        {name: 'QA', id: 8, start: false, end: false, idle: false, wipLimit: 1},
+        {name: 'RF Ops', id: 9, start: false, end: false, idle: true, wipLimit: 4},
+        {name: 'Ops', id: 10, start: false, end: false, idle: false, wipLimit: 1},
         {name: 'Done', id: 11, start: false, end: true, idle: true, wipLimit: 99}
     ];
 
-    flow.historicals = [];
+    flow.historicalTimes = [];
+    flow.historicalQueues = [];
     flow.done = false;
     flow.running = false;
+    flow.maxItemProgress = 1;
+    flow.minItemProgress = 1;
 
-    flow.workSizes = [1, 2, 3, 5, 8, 13];
+    flow.workSizes = [1, 2, 3, 4, 5, 8, 13];
+    flow.queueSizes = {3: [], 5: [], 7: [], 9: []};
 
     flow.items = [
-      {name: "Widget a", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget b", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget c", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget o", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget p", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget h", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget i", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget q", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget z", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget f", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget l", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget k", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget t", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
-      {name: "Widget d", columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget a", id: 1, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget b", id: 2, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget c", id: 3, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget o", id: 4, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget p", id: 5, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget h", id: 6, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget i", id: 7, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget q", id: 8, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget z", id: 9, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget f", id: 10, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget l", id: 11, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget k", id: 12, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget t", id: 13, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
+      {name: "Widget d", id: 14, columnId: 1, timestamp: 0, times: {active: 0, idle: 0}, workRemaining: _.sample(flow.workSizes)},
     ];
 
     flow.resetBoard = function() {
       if (flow.running) { return; };
       if (flow.done) {
-        flow.historicals.push([flow.totalActiveTime(), flow.totalIdleTime(), flow.totalTime(), flow.totalIdleTimePercentage()]);
+        flow.historicalTimes.push([flow.totalActiveTime(), flow.totalIdleTime(), flow.totalTime(), flow.totalIdleTimePercentage()]);
+        flow.historicalQueues.push([
+          flow.averageQueueSize(flow.columnById(3)),
+          flow.averageQueueSize(flow.columnById(5)),
+          flow.averageQueueSize(flow.columnById(7)),
+          flow.averageQueueSize(flow.columnById(9))]);
       }
       else {
         $interval.cancel(flow.timer);
       }
       _.each(flow.items, flow.resetItem);
+      flow.queueSizes = {3: [], 5: [], 7: [], 9: []};
+      flow.maxItemProgress = 1;
+      flow.minItemProgress = 1;
       flow.done = false;
       flow.running = false;
     };
@@ -87,6 +99,7 @@ angular.module('flowApp', [])
         console.log("Done!");
       }
       else {
+        flow.recordQueueSizes();
         flow.tickWorkers();
       }
     };
@@ -95,17 +108,18 @@ angular.module('flowApp', [])
       let walkTheBoard = _.sortBy(flow.workColumns(), 'id').reverse();
       _.each(walkTheBoard, function(workColumn) {
         console.log("ticking column " + workColumn.name);
-        _.each(flow.itemsInColumn(workColumn), function(item) {
+        let workItem = _.first(_.sortBy(flow.itemsInColumn(workColumn), 'id'));
+        if (workItem) {
           console.log("working...");
-          flow.doWork(item);
-          if (item.workRemaining == 0) {
+          flow.doWork(workItem);
+          if (workItem.workRemaining == 0) {
             console.log("...done!");
-            flow.moveItem(item);
+            flow.moveItem(workItem);
           }
           else {
             console.log("...not done!");
           }
-        });
+        }
         if (workColumn.id > 1) {
           while (flow.columnUnderWipLimit(workColumn)) {
             let pullColumn = flow.columnById(workColumn.id - 1);
@@ -123,7 +137,7 @@ angular.module('flowApp', [])
     };
 
     flow.doWork = function(item) {
-      let productivity = _.sample([0, 1, 2]);
+      let productivity = _.sample([0, 0, 1, 2]);
       console.log(item.workRemaining + " work remaining on " + item.name + ", productivity of " + productivity);
       let delta = item.workRemaining - productivity;
       if (delta < 0) {
@@ -132,6 +146,44 @@ angular.module('flowApp', [])
       else {
         item.workRemaining = delta;
       }
+    };
+
+    flow.recordQueueSizes = function() {
+      _.each(flow.queueColumns(), function(col) {
+        // don't record queues outside of the items in progress
+        if (col.id >= flow.minItemProgress && col.id <= flow.maxItemProgress) {
+          let size = flow.itemsInColumn(col).length;
+          flow.queueSizes[col.id].push(size);
+        }
+      });
+    };
+
+    flow.queueColumns = function() {
+      let qs = _.filter(flow.columns, function(col) {
+        return col.idle && !col.start && !col.end;
+      });
+      return qs;
+    };
+
+    flow.idleColumns = function() {
+      return _.filter(flow.columns, function(col) {
+        return col.idle;
+      });
+    };
+
+    flow.itemsRemainingForColumn = function(column) {
+      let columns = _.filter(flow.columns, function(col) {
+        return col.id <= column.id;
+      });
+      let items = _.reduce(columns, function(memo, col) { return memo + flow.itemsInColumn(col); }, []);
+      return items;
+    }
+
+    flow.averageQueueSize = function(column) {
+      let samples = flow.queueSizes[column.id];
+      let total = _.reduce(samples, function(memo, itm) { return memo + itm; }, 0);
+      if (samples.length == 0) { return; }
+      return (total / samples.length).toFixed(2);
     };
 
     flow.columnUnderWipLimit = function(column) {
@@ -165,8 +217,12 @@ angular.module('flowApp', [])
         }
       }
       item.timestamp = newTimestamp;
-      item.columnId = fromColumn.id + 1;
+      item.columnId = toColumn.id;
       item.workRemaining = _.sample(flow.workSizes);
+      flow.maxItemProgress = _.max([flow.maxItemProgress, toColumn.id]);
+      if (flow.itemsRemainingForColumn(fromColumn).length == 0) {
+        flow.minItemProgress = toColumn.id;
+      }
       console.log("...success!");
     };
 
